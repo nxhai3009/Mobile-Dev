3f4b88decddafda0eaef1e25bbe2a607a12524a3
nxhai3009 <xuanhaiminhphu@gmail.com>
2025-10-12 16:34:40 +0700

    Thuc hanh 3

---
+++ TH3/product_api/routes/api.php
@@ -0,0 +1,11 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Api\ProductController;
+
+Route::get('/products', [ProductController::class, 'index']);
+Route::post('/products', [ProductController::class, 'store']);
+Route::get('/products/{product}', [ProductController::class, 'show']);
+Route::put('/products/{product}', [ProductController::class, 'update']);
+Route::delete('/products/{product}', [ProductController::class, 'destroy']);
